c545b513422ca38f7181de7335cff5057635012f
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2024-09-19 03:22:38 +0330

    Add string representations of models.

--- auctions/models.py
+++ auctions/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self) -> str:
+        return self.title
+
 
 class Listing(models.Model):
     title = models.CharField(max_length=255)
@@ -17,13 +20,17 @@ class Listing(models.Model):
     starting_bid = models.DecimalField(max_digits=7, decimal_places=2)
     image_url = models.URLField(blank=True, null=True)
     active = models.BooleanField(default=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     category = models.ForeignKey(
         Category,
         blank=True,
         null=True,
         on_delete=models.PROTECT,
+        related_name="listings",
     )
+
+    def __str__(self) -> str:
+        return self.title
 
 
 class Bid(models.Model):
@@ -31,13 +38,22 @@ class Bid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return f"{self.user} - {self.listing}"
+
 
 class WatchList(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self) -> str:
+        return f"{self.user} - {self.listing}"
+
 
 class Comment(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.user} - {self.listing}"
