e362371ab02d0f2371db16a40b0a1e5467eca9a2
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2024-09-19 02:57:18 +0330

    Implement models. Make initial migrations. Run initial migrations.

--- auctions/models.py
+++ auctions/models.py
@@ -6,3 +6,38 @@ class User(AbstractUser):
     pass
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=7, decimal_places=2)
+    image_url = models.URLField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category,
+        blank=True,
+        null=True,
+        on_delete=models.PROTECT,
+    )
+
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=7, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+
+class WatchList(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
